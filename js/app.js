d3f53bf7f7fd1048513ef2e9002dd81280c8d74a
yechua-silva <yechua_silva@outlook.cl>
2022-12-21 17:38:06 -0300

    Agrege validacion para el textArea

--- js/app.js
+++ js/app.js
@@ -7,11 +7,32 @@ const tweets = [];
 //agrega los tweets
 const agregarTweet = e => {
     e.preventDefault();
-    console.log('agregando tweet');
+    const tweet = document.querySelector('#tweet').value
+    //validacion
+    if (tweet === '') {
+        mostrarError('Una tarea no puede estar vacia')
+        return;//evita que se ejecuten mas lineas de codigo
+    }
+}
+//mostrar mensaje de error
+const mostrarError = (error) => {
+    const mensajeError = document.createElement('P')
+    mensajeError.textContent = error;
+    mensajeError.classList.add('error');
+
+    //insertar en el contenido
+    const contenido = document.querySelector('#contenido');
+    contenido.appendChild(mensajeError);
+
+    //Elimina la alerta despues de 3 segundos
+    setTimeout(() => {
+        mensajeError.remove();
+    }, 3000)
 }
 //EVENTS LISTENERS
 const eventListener = () => {
     //formulario escucha cuando se hace el submit
     formulario.addEventListener('submit', agregarTweet)
+
 }
 eventListener();
